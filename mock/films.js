7aa1b73c2f0de2dacc6220e7f95297c80e1f892c
FluckyMENDES <fluckymendes@gmail.com>
2021-02-20 11:15:33 +0300

    Added a generation of the mock data for films. The film cards are displayed using mock data. The details popup is displayed. Change the linting-config.

---
+++ mock/films.js
@@ -0,0 +1,152 @@
+import {
+  getRandomInteger,
+  getArrayRandomItem,
+  getRandomDate,
+  createRandomLengthUniqArray,
+} from './../src/components/utils/utils';
+
+const FIRST_FILM_DATE = new Date(`1950-01-01`);
+const END_FILM_DATE = new Date(`1990-12-31`);
+
+const titles = [
+  'Страх и ненависть в Лас-Вегасе',
+  'Армагеддон',
+  'Криминальное чтиво',
+  'Всё или ничено',
+  'Аладдин',
+  'Мгла',
+  'Дьявол носит Prada',
+  'Девять жизней',
+];
+
+const posters = [
+  'made-for-each-other.png',
+  'popeye-meets-sinbad.png',
+  'sagebrush-trail.jpg',
+  'santa-claus-conquers-the-martians.jpg',
+  'the-dance-of-life.jpg',
+  'the-great-flamarion.jpg',
+  'the-man-with-the-golden-arm.jpg',
+];
+
+const directors = [
+  'Крис Коламбус',
+  'Квентин Тарантино',
+  'Эльдар Рязанов',
+  'Джордж Лукас',
+  'Робер Земейкис',
+  'Питер Джексон',
+];
+
+const writers = [
+  'Вернон Дурсль',
+  'Анджелика Джонс',
+  'Златоуст Локхарт',
+  'Рональд Уизли',
+  'Люциус Малфой',
+  'Сириус Блек',
+  'Годрик Гриффиндор',
+  'Рита Скитер',
+  'Седрик Диггори',
+  'Колин Криви',
+  'Аргус Филч',
+];
+
+const actors = [
+  'Антон Галыгин',
+  'Илья Ильин',
+  'Александр Степанов',
+  'Алексей Ежов',
+  'Михаил Гаврилин',
+  'Светлана Волчанина',
+  'Татьяна Чудакова',
+  'Алексей Исаев',
+  'Александр Захаров',
+  'Рахим Халекпур',
+  'Мария Вилочкина',
+  'Алёна Грачёва',
+  'Татьяна Сенина',
+  'Максим Кононов',
+  'Кирилл Комлев',
+  'Сергей Гуров',
+];
+
+const comments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const ageLimits = ['0+', '6+', '12+', '16+', '18+'];
+
+const countries = [
+  'Russia',
+  'USA',
+  'France',
+  'England',
+  'Ireland',
+  'Spain',
+  'Italy',
+  'Sweeden',
+  'Mexico',
+  'Canada',
+  'Germany',
+  'Maroco',
+  'Thailand',
+  'China',
+];
+
+const genres = [
+  `Comedy`,
+  `Action`,
+  `Drama`,
+  'Thriller',
+  'Horror',
+  'Musical',
+  'Erotic',
+  'Documentry',
+  'Romance',
+  'Film-Noir',
+  'Mystery',
+  'Western',
+];
+
+const generateMockFilmsData = (count) => {
+  const result = [];
+
+  for (let i = 0; i < count; i++) {
+    const film = {
+      id: i,
+      comments: createRandomLengthUniqArray(comments, 15),
+      props: {
+        title: getArrayRandomItem(titles),
+        originalTitle: `Original title of the movie`,
+        rating: `${getRandomInteger(0, 9)}.${getRandomInteger(0, 9)}`,
+        posterUrl: getArrayRandomItem(posters),
+        releaseDate: getRandomDate(FIRST_FILM_DATE, END_FILM_DATE),
+        director: getArrayRandomItem(directors),
+        writers: createRandomLengthUniqArray(writers, 6),
+        actors: createRandomLengthUniqArray(actors, 15),
+        genres: createRandomLengthUniqArray(genres, 4),
+        durationMinutes: getRandomInteger(30, 200),
+        country: getArrayRandomItem(countries),
+        ageLimit: createRandomLengthUniqArray(ageLimits, 1),
+        desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum. Scripta periculis ei eam, te pro movet reformidans.
+        Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri
+        persius vituperata ei nec.
+        `,
+      },
+      userProps: {
+        willWatch: !!Math.round(Math.random()),
+        isWatched: !!Math.round(Math.random()),
+        isFavorite: !!Math.round(Math.random()),
+      },
+    };
+
+    result.push(film);
+  }
+
+  return result;
+};
+
+export default generateMockFilmsData;
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import { render } from "./components/utils/utils";
+import {render} from './components/utils/utils';
 
-import Profile from "./components/profile/profile";
-import MainNavigation from "./components/main-navigation/main-navigation";
-import Sorting from "./components/sorting/sorting";
-import TotalFilmsCount from "./components/total-film-count/total-film-count";
-import MainContent from "./components/main-content/main-content";
+import Profile from './components/profile/profile';
+import MainNavigation from './components/main-navigation/main-navigation';
+import Sorting from './components/sorting/sorting';
+import TotalFilmsCount from './components/total-film-count/total-film-count';
+import MainContent from './components/main-content/main-content';
+import FilmCard from './components/film-card/film-card';
+import DetailsPopup from './components/details-popup/details-popup';
+
+import generateMockFilmsData from '../mock/films';
+
+const SHOWING_FILMS_COUNT_ON_START = 5;
+
+// Генерация моковых данных
+
+const films = generateMockFilmsData(20);
+
+const getfavoriteFilms = (filmsData) => {
+  return filmsData.slice().filter((film) => film.userProps.isFavorite === true);
+};
+
+const watchlistFilms = films.slice().filter((film) => film.userProps.willWatch === true);
 
 const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, Profile());
@@ -12,9 +28,63 @@ render(siteHeaderElement, Profile());
 const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, MainNavigation());
 render(siteMainElement, Sorting());
-render(siteMainElement, MainContent());
+render(siteMainElement, MainContent(films, SHOWING_FILMS_COUNT_ON_START));
 
-const siteFooterStatistictElement = document.querySelector(
-  `.footer__statistics`
-);
+const siteFooterStatistictElement = document.querySelector(`.footer__statistics`);
 render(siteFooterStatistictElement, TotalFilmsCount());
+
+/**
+ * Popup
+ */
+
+let popup = document.querySelector('.film-details');
+
+const hadleEscKeydown = (e) => {
+  if (e.code === 'Escape') {
+    popup = document.querySelector('.film-details');
+    if (popup) {
+      popup.remove();
+    }
+  }
+};
+
+document.addEventListener('click', (evt) => {
+  if (
+    evt.target.closest('.film-card__poster') ||
+    evt.target.closest('.film-card__title') ||
+    evt.target.closest('.film-card__comments')
+  ) {
+    const filmCard = evt.target.closest('.film-card');
+    const id = filmCard.dataset.id;
+
+    render(siteMainElement, DetailsPopup(films[id]));
+    document.addEventListener('keydown', hadleEscKeydown);
+  } else if (evt.target.closest('.film-details__close-btn')) {
+    popup = document.querySelector('.film-details');
+    if (popup) {
+      popup.remove();
+      document.removeEventListener('keydown', hadleEscKeydown);
+    }
+  }
+});
+
+/**
+ * Show more
+ */
+
+const filmList = document.querySelector('.films-list__container');
+const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
+
+let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+
+const showMoreBtn = document.querySelector('.films-list__show-more');
+showMoreBtn.addEventListener('click', () => {
+  const prevFilmsCount = showingFilmsCount;
+  showingFilmsCount += SHOWING_FILMS_COUNT_BY_BUTTON;
+
+  films.slice(prevFilmsCount, showingFilmsCount).forEach((film) => render(filmList, FilmCard(film)));
+
+  if (showingFilmsCount >= films.length) {
+    showMoreBtn.remove();
+  }
+});
